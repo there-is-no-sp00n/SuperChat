1e09c5a3484ec8d2667aa659b0f568cfde3dda7f
there-is-no-sp00n <aninda.zaman@gmail.com>
2017-07-13 14:56:52 -0500

    Create User.h

---
+++ User.h
@@ -0,0 +1,18 @@
+#ifndef USER_H
+#define USER_H
+
+class User
+{
+    public:
+          User()
+          
+          
+          void set_user();
+
+
+    private:
+    
+          string nick;
+          string UUID;
+          
+}
